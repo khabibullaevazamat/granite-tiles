62800b97152cdb5f942a6df745a365f33e163a9b
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-07-17 12:39:50 +0500

    commit v4

--- src/routes/catalog.tsx
+++ src/routes/catalog.tsx
@@ -1,16 +1,37 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/catalog')({
+export const Route = createFileRoute("/catalog")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
     <>
-      <div className="max-w-[600px] mx-auto p-4">
+      <div>
+        <div className="mb-4">
+          <Button variant="outline" asChild>
+            <Link className="" to="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M6 8L2 12L6 16" />
+                <path d="M2 12H22" />
+              </svg>
+            </Link>
+          </Button>
+        </div>
         <div className="grid grid-cols-1 gap-8">
           {[...Array(66)].map((_, i) => {
-            const num = String(i + 1).padStart(2, '0')
+            const num = String(i + 1).padStart(2, "0");
             return (
               <img
                 key={num}
@@ -18,10 +39,10 @@ function RouteComponent() {
                 src={`/${num}.jpg`}
                 className="block h-auto max-w-full border-none"
               />
-            )
+            );
           })}
         </div>
       </div>
     </>
-  )
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,10 +10,41 @@ function App() {
     <div className="">
       <div className="flex flex-col gap-4">
         <Button className="uppercase py-6" variant="outline" asChild>
-          <Link to="/catalog">Katalog</Link>
+          <Link to="/catalog">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M18 22H4a2 2 0 0 1-2-2V6" />
+              <path d="m22 13-1.296-1.296a2.41 2.41 0 0 0-3.408 0L11 18" />
+              <circle cx="12" cy="8" r="2" />
+              <rect width="16" height="16" x="6" y="2" rx="2" />
+            </svg>
+            Katalog
+          </Link>
         </Button>
         <Button className="uppercase py-6" variant="outline" asChild>
-          <a target="_blank" href="tel:998971709090" rel="noopener noreferrer">
+          <a target="_blank" href="tel:+998971709090" rel="noopener noreferrer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384" />
+            </svg>
             +998971709090
           </a>
         </Button>
@@ -23,6 +54,21 @@ function App() {
             rel="noopener noreferrer"
             href="https://instagram.com/tile_kafel"
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+              <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+            </svg>
             Instagram
           </a>
         </Button>
@@ -32,6 +78,20 @@ function App() {
             rel="noopener noreferrer"
             href="https://t.me/keramagranit"
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z" />
+              <path d="m21.854 2.147-10.94 10.939" />
+            </svg>
             Telegram
           </a>
         </Button>
