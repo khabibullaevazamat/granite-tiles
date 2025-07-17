15c20b895c3d1052a1694327be20cb9d2e3cc4bf
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-07-17 10:03:00 +0500

    commit v2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+// import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,13 +12,13 @@ export default defineConfig({
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  // },
+  // resolve: {
+  //   alias: {
+  //     '@': resolve(__dirname, './src'),
+  //   },
+  // },
 })
