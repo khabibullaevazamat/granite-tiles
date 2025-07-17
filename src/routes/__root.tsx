687c61e6fa470074f56991c998adb7e2bc292e87
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-07-17 15:56:53 +0500

    cmt v11

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,7 +4,7 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="max-w-[600px] mx-auto p-4">
+      <div className="bg-[url('/bg.jpg')] bg-cover bg-center h-screen max-w-[600px] mx-auto p-4">
         <Outlet />
       </div>
       <TanStackRouterDevtools />
