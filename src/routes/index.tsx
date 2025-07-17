42f81a7d5774d1d2b011a9c624289b21ca59d391
azamatcto <azamatkhabibullaevcto@gmail.com>
2025-07-17 14:23:39 +0500

    cmt v9

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="">
       <div className="flex flex-col gap-4">
-        <Button className="uppercase py-6" variant="outline" asChild>
+        <Button className="text-lg font-bold uppercase py-6" variant="outline" asChild>
           <Link to="/catalog">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -21,6 +21,7 @@ function App() {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
+              className="size-6"
             >
               <path d="M18 22H4a2 2 0 0 1-2-2V6" />
               <path d="m22 13-1.296-1.296a2.41 2.41 0 0 0-3.408 0L11 18" />
@@ -30,25 +31,26 @@ function App() {
             Katalog
           </Link>
         </Button>
-        <Button className="uppercase py-6" variant="outline" asChild>
+        <Button className="text-lg font-bold uppercase py-6" variant="outline" asChild>
           <a target="_blank" href="tel:+998971709090" rel="noopener noreferrer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
+              width="28"
+              height="28"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
+              className="size-6"
             >
               <path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384" />
             </svg>
             +998971709090
           </a>
         </Button>
-        <Button className="uppercase py-6" variant="outline" asChild>
+        <Button className="text-lg font-bold uppercase py-6" variant="outline" asChild>
           <a
             target="_blank"
             rel="noopener noreferrer"
@@ -64,6 +66,7 @@ function App() {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
+              className="size-6"
             >
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
               <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
@@ -72,7 +75,7 @@ function App() {
             Instagram
           </a>
         </Button>
-        <Button className="uppercase py-6" variant="outline" asChild>
+        <Button className="text-lg font-bold uppercase py-6" variant="outline" asChild>
           <a
             target="_blank"
             rel="noopener noreferrer"
@@ -88,6 +91,7 @@ function App() {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
+              className="size-6"
             >
               <path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z" />
               <path d="m21.854 2.147-10.94 10.939" />
@@ -95,7 +99,7 @@ function App() {
             Telegram
           </a>
         </Button>
-        <Button className="uppercase py-6" variant="outline" asChild>
+        <Button className="text-lg font-bold uppercase py-6" variant="outline" asChild>
           <a
             target="_blank"
             rel="noopener noreferrer"
@@ -111,6 +115,7 @@ function App() {
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
+              className="size-6"
             >
               <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
               <circle cx="12" cy="10" r="3" />
